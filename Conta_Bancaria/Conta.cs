aa10799c1e26bd7378a59fc241f2c1185615301e
Diego-nrd <diego-11-0@hotmail.com>
2022-08-21 22:48:54 -0300

    Exercicio do Curso de C# ASP.NET

---
+++ Conta_Bancaria/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+namespace Conta_Bancaria
+{
+    class Conta
+    {
+        public int Nconta { get; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        private double _taxa = 5.00;
+
+        public Conta(int nconta, string nome)
+        {
+            Nconta = nconta;
+            Nome = nome;
+        }
+
+        public Conta(int nconta, string nome, double valorInicial) : this(nconta, nome)
+        {
+            Depositar(valorInicial);
+        }
+        public void Depositar(double saldo)
+        {
+            Saldo += saldo;
+        }
+
+        public void Sacar(double saldo)
+        {
+            Saldo -= saldo + _taxa;
+        }
+
+        public override string ToString()
+        {
+            return Nconta +
+                ", Titular: " +
+                Nome +
+                ", Saldo: $" +
+                Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Conta_Bancaria/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conta_Bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre com Número da Conta: ");
+            int nconta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o Nome do Titular: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (s/n) ?");
+            char opcao = char.Parse(Console.ReadLine());
+
+            conta = new Conta(nconta, nome);
+
+            if (opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Entre com o valor de déposito inicial: ");
+                double dpInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Depositar(dpInicial);
+
+                Console.WriteLine("\nDados da Conta:\nConta: {0}", conta);
+
+                Console.Write("\nEntre com um valor de Depósito: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Depositar(saldo);
+
+                Console.WriteLine("Dados da conta Atualizado:");
+                Console.Write("Conta: {0}\n", conta);
+
+                Console.Write("\nEntre com um valor para saque: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Sacar(saldo);
+
+                Console.WriteLine("Dados da conta Atualizado:");
+                Console.Write("Conta: {0}\n", conta);
+
+            }
+            else if (opcao == 'n' || opcao == 'N')
+            {
+
+                Console.WriteLine("\nDados da Conta:\nConta: {0}", conta);
+
+                Console.Write("\nEntre com um valor de Depósito: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Depositar(saldo);
+
+                Console.WriteLine("Dados da conta Atualizado:");
+                Console.Write("Conta: {0}\n", conta);
+
+                Console.Write("\nEntre com um valor para saque: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Sacar(saldo);
+
+                Console.WriteLine("Dados da conta Atualizado:");
+                Console.Write("Conta: {0}\n", conta);
+            }
+            else
+            {
+                Console.WriteLine("Opção Invalida!!");
+            }
+
+        }
+    }
+}
